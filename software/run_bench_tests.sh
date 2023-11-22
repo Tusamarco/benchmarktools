d85d7f8b022934532dd5efe16b46e5ade37c15fa
Marco Tusa <tusamarco@gmail.com>
2023-11-22 17:04:36 +0100

    no message

--- software/run_bench_tests.sh
+++ software/run_bench_tests.sh
@@ -182,13 +182,13 @@ echo "Tables: $TPCc_TABLES"
 nc -w 1 -z $host $port
 if [ $? -ne 0 ] ; then
     echo "[ERROR] Mysql did not start correctly ($host : $port)"
-    if [ "$dryrun" == "true" ]; then
+    if [ ! "$dryrun" == "true" ]; then
 	    echo "[ERROR] Mysql did not start correctly ($host : $port)" >> "${LOGFILE}"
 	fi
 #  exit 1
 else
   echo "[OK] Mysql running correctly" 
-  if [ "$dryrun" == "true" ]; then
+  if [ ! "$dryrun" == "true" ]; then
 	  echo "[OK] Mysql running correctly" >> "${LOGFILE}"
   fi
 fi
