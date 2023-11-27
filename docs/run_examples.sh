274323d0caf3d23aa60e2d31bc3a8a94b2d0c106
Marco Tusa <tusamarco@gmail.com>
2023-11-27 15:58:39 +0100

    no message

--- docs/run_examples.sh
+++ docs/run_examples.sh
@@ -13,11 +13,13 @@ execute two runs of each test:
     bash run_bench_tests.sh --test PS8034_small  --testname sysbench --command run  --filter_subtest select  --THREADS "1 2 4 8 16 32 64 128 256 512 1024" --TIME 600 --sysbench_test_dimension small  --host 10.0.68.39 --port 3307 --schemaname windmills_small
     
     will be as command 
-    
+
+#!/bin/bash
+bin_path="/opt/tools/benchmarktools/software"    
     for dimension in small large; do
         for type in select write;do
             for run in 1 2;do 
-                bash run_bench_tests.sh --test PS8034_${dimension}_${type}_${run}  --testname sysbench --command run  --filter_subtest ${type}  --THREADS "1 2 4 8 16 32 64 128 256 512 1024" --TIME 600 --sysbench_test_dimension ${dimension}  --host 10.0.68.39 --port 3307 --schemaname windmills_${dimension}
+                bash $bin_path/run_bench_tests.sh --test PS8034_${dimension}_${type}_${run}  --testname sysbench --command run  --filter_subtest ${type}  --THREADS "1 2 4 8 16 32 64 128 256 512 1024" --TIME 600 --sysbench_test_dimension ${dimension}  --host 10.0.68.39 --port 3307 --schemaname windmills_${dimension}
             done;       
         done;
     done;
