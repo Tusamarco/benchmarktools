4fad0b0df6a36e024d40be9ae481accddc7b2c1f
Marco Tusa <tusamarco@gmail.com>
2023-11-27 16:01:22 +0100

    no message

--- docs/run_examples.sh
+++ docs/run_examples.sh
@@ -17,8 +17,11 @@ execute two runs of each test:
 #!/bin/bash
 bin_path="/opt/tools/benchmarktools/software"    
     for dimension in small large; do
+        echo "Running dimension: ${dimension}"
         for type in select write;do
+            echo "Running type: ${type}"
             for run in 1 2;do 
+                echo "Running round: ${run}"
                 bash $bin_path/run_bench_tests.sh --test PS8034_${dimension}_${type}_${run}  --testname sysbench --command run  --filter_subtest ${type}  --THREADS "1 2 4 8 16 32 64 128 256 512 1024" --TIME 600 --sysbench_test_dimension ${dimension}  --host 10.0.68.39 --port 3307 --schemaname windmills_${dimension}
             done;       
         done;
