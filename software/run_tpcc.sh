d5805f444d9d67e0c8f4f74a783d72022f8da004
Marco Tusa <tusamarco@gmail.com>
2023-12-19 14:30:52 +0100

    no message

--- software/run_tpcc.sh
+++ software/run_tpcc.sh
@@ -5,8 +5,8 @@ for type in run_tpcc_RepeatableRead run_tpcc_ReadCommitted ; do
 	echo "Running type: ${type}"
 	for run in 1 ; do
 		echo "Running round: ${run}"
-		echo "RUNNING: $bin_path/run_bench_tests.sh --test ${testidentifyer}_${type}_${run}  --testname tpcc --command run  --filter_subtest ${type}  --THREADS \"1 2 4 8 16 32 64 128 256 512 1024\" --TIME 900 --sysbench_test_dimension ${dimension}  --host 10.0.68.39 --port 3307 --schemaname tpcc"
+		echo "RUNNING: $bin_path/run_bench_tests.sh --test ${testidentifyer}_${type}_${run}  --testname tpcc --command run  --filter_subtest ${type}  --THREADS \"1 2 4 8 16 32 64 128 256 512 1024\" --TIME 900  --host 10.0.68.39 --port 3307 --schemaname tpcc"
 
-		bash $bin_path/run_bench_tests.sh --test ${testidentifyer}_${type}_${run}  --testname tpcc --command run  --filter_subtest ${type}  --THREADS "1 2 4 8 16 32 64 128 256 512 1024" --TIME 900 --sysbench_test_dimension ${dimension}  --host 10.0.68.39 --port 3307 --schemaname tpcc
+		bash $bin_path/run_bench_tests.sh --test ${testidentifyer}_${type}_${run}  --testname tpcc --command run  --filter_subtest ${type}  --THREADS "1 2 4 8 16 32 64 128 256 512 1024" --TIME 900 --host 10.0.68.39 --port 3307 --schemaname tpcc
 	done;
 done;
