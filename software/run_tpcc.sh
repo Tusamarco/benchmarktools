6c5008df2b76aac17efff92f9f60ef568c3cc533
Marco Tusa <tusamarco@gmail.com>
2024-01-30 15:14:27 +0100

    adding handler for service name for PMM

--- software/run_tpcc.sh
+++ software/run_tpcc.sh
@@ -6,9 +6,15 @@ PMMURL=${4:-"http://admin:admin@127.0.0.1"}
 HAVEPMM=${5:-"false"}
 PMMNODENAME=${6:-"bench"}
 havePMM=""
+PMMSERVICENAME=${7:-""}
+
+havePMM=""
 
 if [ "$HAVEPMM" = "true" ]; then
 	havePMM="--havePMM"
+	if [ ! "$PMMSERVICENAME" == "" ]; then
+	   PMMSERVICENAME="--pmm_service_name $PMMSERVICENAME"  
+	fi
 fi
 
 bin_path="/opt/tools/benchmarktools/software"
@@ -16,8 +22,8 @@ for type in run_tpcc_RepeatableRead run_tpcc_ReadCommitted ; do
 	echo "Running type: ${type}"
 	for run in 1 ; do
 		echo "Running round: ${run}"
-		echo "RUNNING: $bin_path/run_bench_tests.sh --test ${testidentifyer}_${type}_${run}  --testname tpcc --command run  --filter_subtest ${type}  --THREADS \"1 2 4 8 16 32 64 128 256 512 1024\" --TIME 900  --host ${HOST} --port $PORT --schemaname tpcc $havePMM --pmm_url $PMMURL --pmm_node_name $PMMNODENAME"
+		echo "RUNNING: $bin_path/run_bench_tests.sh --test ${testidentifyer}_${type}_${run}  --testname tpcc --command run  --filter_subtest ${type}  --THREADS \"1 2 4 8 16 32 64 128 256 512 1024\" --TIME 900  --host ${HOST} --port $PORT --schemaname tpcc $havePMM --pmm_url $PMMURL --pmm_node_name $PMMNODENAME $PMMSERVICENAME"
 
-		bash $bin_path/run_bench_tests.sh --test ${testidentifyer}_${type}_${run}  --testname tpcc --command run  --filter_subtest ${type}  --THREADS "1 2 4 8 16 32 64 128 256 512 1024" --TIME 900 --host ${HOST} --port $PORT --schemaname tpcc $havePMM --pmm_url $PMMURL --pmm_node_name $PMMNODENAME
+		bash $bin_path/run_bench_tests.sh --test ${testidentifyer}_${type}_${run}  --testname tpcc --command run  --filter_subtest ${type}  --THREADS "1 2 4 8 16 32 64 128 256 512 1024" --TIME 900 --host ${HOST} --port $PORT --schemaname tpcc $havePMM --pmm_url $PMMURL --pmm_node_name $PMMNODENAME $PMMSERVICENAME
 	done;
 done;
