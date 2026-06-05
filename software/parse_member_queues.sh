5f9601d9e6da8fab1795838db87cfef63372bd10
Marco Tusa <tusamarco@gmail.com>
2026-06-05 13:47:09 +0200

    Add host name to catch the moment of the primary change

--- software/parse_member_queues.sh
+++ software/parse_member_queues.sh
@@ -7,8 +7,7 @@
 # the Free Software Foundation, either version 3 of the License.
 
 # Given the query:
-# while true; do mysql -u<> -p<> -h <> -e "SELECT a.MEMBER_ROLE, a.MEMBER_ID, a.MEMBER_HOST, b.COUNT_TRANSACTIONS_IN_QUEUE AS certifier_queue, b.COUNT_TRANSACTIONS_REMOTE_IN_APPLIER_QUEUE AS applier_queue, ROUND((b.COUNT_TRANSACTIONS_REMOTE_IN_APPLIER_QUEUE / NULLIF(@@GLOBAL.group_replication_flow_control_applier_threshold, 0)) * 100, 2) AS threshold_pct, b.COUNT_TRANSACTIONS_LOCAL_PROPOSED AS proposed, b.COUNT_TRANSACTIONS_REMOTE_APPLIED AS applied FROM performance_schema.replication_group_members AS a JOIN performance_schema.replication_group_member_stats AS b ON a.MEMBER_ID = b.MEMBER_ID;" |tee -a `pwd`/members_lag.log;sleep 1;done;
-
+# while true; do mysql -u<> --p<> -h <> -e "SELECT NOW() AS log_timestamp, IF(a.MEMBER_ROLE = 'PRIMARY', @@global.hostname, 'n/a') AS hostname, @@GLOBAL.super_read_only AS super_read_only, a.MEMBER_ROLE, a.MEMBER_ID, a.MEMBER_HOST, b.COUNT_TRANSACTIONS_IN_QUEUE AS certifier_queue, b.COUNT_TRANSACTIONS_REMOTE_IN_APPLIER_QUEUE AS applier_queue, ROUND((b.COUNT_TRANSACTIONS_REMOTE_IN_APPLIER_QUEUE / NULLIF(@@GLOBAL.group_replication_flow_control_applier_threshold, 0)) * 100, 2) AS threshold_pct, b.COUNT_TRANSACTIONS_LOCAL_PROPOSED AS proposed, b.COUNT_TRANSACTIONS_REMOTE_APPLIED AS applied FROM performance_schema.replication_group_members AS a JOIN performance_schema.replication_group_member_stats AS b ON a.MEMBER_ID = b.MEMBER_ID;" | tee -a `pwd`/members_lag.log; sleep 1; done;
 
 
 if [ "$#" -ne 2 ]; then
@@ -38,12 +37,13 @@ BEGIN {
     # Reset the per-block role counters
     for (r in role_count) role_count[r] = 0
     
-    # Dynamically find the column indexes including the new additions
+    # Dynamically find the column indexes including the new hostname addition
     for (i = 1; i <= NF; i++) {
         # Clean up any potential carriage returns
         gsub(/\r$/, "", $i)
         
         if ($i == "log_timestamp") ts_idx = i
+        if ($i == "hostname") hn_idx = i
         if ($i == "super_read_only") sro_idx = i
         if ($i == "MEMBER_ROLE") role_idx = i
         if ($i == "MEMBER_HOST") host_idx = i
@@ -62,27 +62,44 @@ role_idx > 0 && $role_idx != "" {
     gsub(/\r$/, "", $NF)
     
     role = $role_idx
-    role_count[role]++
     
-    # Create a unique column prefix for this node (e.g., PRIMARY_1, SECONDARY_1)
-    col_prefix = role "_" role_count[role]
-    
-    # If we discover a new Nth node for this role, register it for the header
-    if (role_count[role] > max_role_count[role]) {
-        max_role_count[role] = role_count[role]
-        ordered_cols[++col_idx] = col_prefix
-    }
-    
-    # Capture the timestamp once per block (since it is the same for all rows in a block)
+    # Capture the timestamp once per block
     if (data[block_num, "ts"] == "") {
         data[block_num, "ts"] = $ts_idx
     }
-    
-    # Buffer the data in a 2D array
-    data[block_num, col_prefix, "sro"] = $sro_idx
-    data[block_num, col_prefix, "host"] = $host_idx
-    data[block_num, col_prefix, "cert"] = $cert_idx
-    data[block_num, col_prefix, "app"] = $app_idx
+
+    # Enforce exactly ONE primary node
+    if (role == "PRIMARY") {
+        col_prefix = "PRIMARY"
+        
+        # Register the PRIMARY columns only once for the header
+        if (!primary_registered) {
+            ordered_cols[++col_idx] = col_prefix
+            primary_registered = 1
+        }
+        
+        # Buffer the PRIMARY specific data
+        data[block_num, col_prefix, "hn"] = $hn_idx
+        data[block_num, col_prefix, "sro"] = $sro_idx
+        data[block_num, col_prefix, "host"] = $host_idx
+        data[block_num, col_prefix, "cert"] = $cert_idx
+        data[block_num, col_prefix, "app"] = $app_idx
+        
+    } else {
+        # Auto-increment SECONDARY nodes (SECONDARY_1, SECONDARY_2, etc.)
+        role_count[role]++
+        col_prefix = role "_" role_count[role]
+        
+        # If we discover a new Nth node for this role, register it for the header
+        if (role_count[role] > max_role_count[role]) {
+            max_role_count[role] = role_count[role]
+            ordered_cols[++col_idx] = col_prefix
+        }
+        
+        # Buffer the SECONDARY queue data
+        data[block_num, col_prefix, "cert"] = $cert_idx
+        data[block_num, col_prefix, "app"] = $app_idx
+    }
 }
 
 # 3. End of file: Print the dynamically built header, then all buffered rows
@@ -94,10 +111,11 @@ END {
     for (i = 1; i <= col_idx; i++) {
         prefix = ordered_cols[i]
         
-        # Only add the MEMBER_HOST and super_read_only headers for PRIMARY nodes
-        if (prefix ~ /^PRIMARY/) {
-            printf "\"%s_MEMBER_HOST\",\"%s_super_read_only\",\"%s_certifier_queue\",\"%s_applier_queue\"%s", prefix, prefix, prefix, prefix, (i == col_idx ? ORS : ",")
+        # Add rich headers strictly for the SINGLE PRIMARY node
+        if (prefix == "PRIMARY") {
+            printf "\"%s_hostname\",\"%s_super_read_only\",\"%s_MEMBER_HOST\",\"%s_certifier_queue\",\"%s_applier_queue\"%s", prefix, prefix, prefix, prefix, prefix, (i == col_idx ? ORS : ",")
         } else {
+            # Standard headers for SECONDARY nodes
             printf "\"%s_certifier_queue\",\"%s_applier_queue\"%s", prefix, prefix, (i == col_idx ? ORS : ",")
         }
     }
@@ -112,11 +130,13 @@ END {
             c_val = (data[b, prefix, "cert"] != "") ? data[b, prefix, "cert"] : "0"
             a_val = (data[b, prefix, "app"] != "") ? data[b, prefix, "app"] : "0"
             
-            # Only print the host and read-only data for PRIMARY nodes
-            if (prefix ~ /^PRIMARY/) {
-                h_val = (data[b, prefix, "host"] != "") ? data[b, prefix, "host"] : "N/A"
+            # Print enriched data for PRIMARY, and simple queues for SECONDARY
+            if (prefix == "PRIMARY") {
+                hn_val = (data[b, prefix, "hn"] != "") ? data[b, prefix, "hn"] : "N/A"
                 sro_val = (data[b, prefix, "sro"] != "") ? data[b, prefix, "sro"] : "N/A"
-                printf "\"%s\",\"%s\",\"%s\",\"%s\"%s", h_val, sro_val, c_val, a_val, (i == col_idx ? ORS : ",")
+                h_val = (data[b, prefix, "host"] != "") ? data[b, prefix, "host"] : "N/A"
+                
+                printf "\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"%s", hn_val, sro_val, h_val, c_val, a_val, (i == col_idx ? ORS : ",")
             } else {
                 printf "\"%s\",\"%s\"%s", c_val, a_val, (i == col_idx ? ORS : ",")
             }
